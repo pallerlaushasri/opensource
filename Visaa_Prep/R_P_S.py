5f446fb788f109eab2e050d9a0154a661f9e2daa
Pallerla Usha Sri <160412546+pallerlaushasri@users.noreply.github.com>
2024-11-14 13:26:49 +0530

    Create R_P_S.py

---
+++ Visaa_Prep/R_P_S.py
@@ -0,0 +1,16 @@
+V,C=list(input().split())
+if(V=='R' and C=='P'):
+    print("Charan")
+elif(V=='P' and C=='S'):
+    print("Charan")
+elif(V=='S' and C=='R'):
+    print("Charan")
+elif(V=='R' and C=='R'):
+    print("NULL")
+elif(V=='P' and C=='P'):
+    print("NULL")
+elif(V=='S' and C=='S'):
+    print("NULL")
+else:
+    print("Vignesh")
+    
